e5b0973baddbb9c57050376f52cc71c95582f466
horusspade <32962788+horusspade@users.noreply.github.com>
2018-08-16 12:43:53 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/fec161d1-6640-468e-8439-b99ab73e3290/token";
+const instanceLocator = "v1:us1:fec161d1-6640-468e-8439-b99ab73e3290";
+
+export { tokenUrl, instanceLocator }
